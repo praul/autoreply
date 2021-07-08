9e14423432d71b04e55be38b798c82aed68caa3e
praul <paul@heimpost.com>
2021-07-08 14:35:36 +0200

    create db folder on launch, moved repliers.py to repliers.py.example

--- script.py
+++ script.py
@@ -6,11 +6,15 @@ sys.path.append('/app')
 from autoreplyer import AutoReplyer
 import threading
 from repliers import v
+from os import makedirs
 
 def start_autoreplier(v):
     autoreplier= AutoReplyer(v)
 
 def startup():
+    try: makedirs('db')
+    except: pass
+    
     processes = []
     for replier in v:
         processes.append( threading.Thread( target=start_autoreplier, args=(replier,) ) )
